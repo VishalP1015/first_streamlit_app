36d3c5d1ab14235852b2da7a5b40b531741d0613
VishalP1015 <156264155+VishalP1015@users.noreply.github.com>
2024-01-29 22:58:11 -0600

    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 
 #Snowflake-related functions
-def get fruit_load_list():
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
@@ -77,7 +77,7 @@ def get fruit_load_list():
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect (**streamlit.secrets["snowflake"])
-  my_data_row = fruit_load_list()
+  my_data_row = get_fruit_load_list()
   streamlit.dataframe (my_data_rows)
 
   
